f5013c38a85c7af1dfdaf9145f86a17e068aac30
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-29 16:56:56 -0300

    remove show method for user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,7 +3,6 @@ import { DestroyUserController } from "@controllers/user/destroy.user.controller
 import { IndexUserController } from "@controllers/user/index.user.controller";
 import { Route } from "@interfaces/route.interface";
 import { Router } from "express";
-import { ShowUserController } from "@controllers/user/show.user.controller";
 import { StoreUserController } from "@controllers/user/store.user.controller";
 import { UpdateUserController } from "@controllers/user/update.user.controller";
 
@@ -13,14 +12,12 @@ export class UserRoutes implements Route {
   public authenticationMiddleware = new AuthenticationMiddleware();
   public storeUserController = new StoreUserController();
   public indexUserController = new IndexUserController();
-  public showUserController = new ShowUserController();
   public updateUserController = new UpdateUserController();
   public destroyUsercontroller = new DestroyUserController();
 
   constructor() {
     this.router.post(`${this.path}`, this.storeUserController.handle);
     this.router.get(`${this.path}`, this.indexUserController.handle);
-    this.router.get(`${this.path}/:id`, this.showUserController.handle);
     this.router.put(
       `${this.path}/:id`,
       this.authenticationMiddleware.check,
